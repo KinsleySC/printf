c91c059a2d2d98841cfa991ae011c2efa416d325
Kinsley SUEDILE COLLET <kinsley.suedile-collet@epitech.eu>
2024-10-22 09:35:49 +0200

    [ADD] mini_printf.c

---
+++ mini_printf.c
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2024
+** mini_printf
+** File description:
+** make_mini_printf
+*/
+
+#include "include/my.h"
+
+void verif(const char *format, int i, va_list list)
+{
+    if (format[i + 1] == 'd' || format[i + 1] == 'i')
+        my_put_nbr(va_arg(list, int));
+    if (format[i + 1] == 's')
+        my_putstr(va_arg(list, char *));
+    if (format[i + 1] == 'c')
+        my_putchar(va_arg(list, int));
+    if (format[i + 1] == '%') {
+        my_putchar('%');
+    }
+}
+
+int mini_printf(const char *format, ...)
+{
+    va_list list;
+    int i = 0;
+
+    va_start(list, format);
+    while (format[i] != '\0') {
+        if (format[i] == '%') {
+            verif(format, i, list);
+            i+= 1;
+        } else {
+            my_putchar(format[i]);
+        }
+        i++;
+    }
+    va_end(list);
+    return 0;
+}
